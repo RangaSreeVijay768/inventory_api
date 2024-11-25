ecc0d3354637d56df7ceb3968c0864d6c091282b
Ranga Sree Vijay <ranga.sreevijay768@gmail.com>
2024-11-25 16:59:42 +0530

    vercel db

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const dbConfig = new Sequelize(process.env.DB_URL, {
+export const dbConfig = new Sequelize(process.env.POSTGRES_URL, {
     dialect: 'postgres',
     logging: false
 });
